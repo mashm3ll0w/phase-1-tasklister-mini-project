ad47b0fd9c42688d4344ed51e63de281e860dbc0
Charles Swaleh <charlswall@live.com>
2022-12-17 11:55:40 +0300

    Completed the basic todo app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const form = document.getElementById("create-task-form")
+  form.addEventListener("submit", createTodoList)
+
+  function createTodoList(e){
+    e.preventDefault();
+    let li = document.createElement("li")
+    li.textContent = e.target["new-task-description"].value
+    document.getElementById("tasks").appendChild(li)
+    e.target["new-task-description"].value = ""
+  }
 });
